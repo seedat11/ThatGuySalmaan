08f018f2df343fc1e68b0214b2b7e5a5bd5c53dc
meridian2 <meridian1@live.com.au>
2014-03-27 12:04:36 +0800

    alan_added
    name added

--- Salmaan-AddYourName/Salmaan-AddYourName/Form1.vb
+++ Salmaan-AddYourName/Salmaan-AddYourName/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ListBox1.Items.Add("Salmaan")
+        ListBox1.Items.Add("Alan")
     End Sub
 End Class
